4aeaeb2811145689cc47c2678796e61dca722694
kojix2 <2xijok@gmail.com>
2024-01-17 21:19:59 +0900

    Refactor mutation and sequence options structs

--- src/wgsim/mutation_options.cr
+++
@@ -1,8 +0,0 @@
-module Wgsim
-  struct MutationOptions
-    property mutation_rate : Float64 = 0.001
-    property indel_fraction : Float64 = 0.15
-    property indel_extension_probability : Float64 = 0.3
-    property reference : Path?
-  end
-end
--- src/wgsim/options.cr
+++ src/wgsim/options.cr
@@ -1,11 +1,31 @@
-require "./mutation_options"
-require "./sequence_options"
-
 module Wgsim
   struct Options
     property command : String = ""
     property seed : UInt64?
     property mut : MutationOptions = MutationOptions.new
     property seq : SequenceOptions = SequenceOptions.new
+  end
+
+  struct MutationOptions
+    property mutation_rate : Float64 = 0.001
+    property indel_fraction : Float64 = 0.15
+    property indel_extension_probability : Float64 = 0.3
+    property reference : Path?
+  end
+
+  struct SequenceOptions
+    property error_rate : Float64 = 0.02
+    property distance : Int32 = 500
+    property std_deviation : Int32 = 50
+    property average_depth : Float64 = 10
+    property size_left : Int32 = 70
+    property size_right : Int32 = 70
+    property mutation_rate : Float64 = 0.001
+    property indel_fraction : Float64 = 0.15
+    property indel_extension_probability : Float64 = 0.3
+    property max_ambiguous_ratio : Float64 = 0.05
+    property reference : Path?
+    property output1 : Path?
+    property output2 : Path?
   end
 end
