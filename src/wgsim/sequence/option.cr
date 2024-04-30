5872c1556c722cf205e513f1def8e6e64b8404b0
kojix2 <2xijok@gmail.com>
2024-04-30 15:09:21 +0900

    Set the default read length from 70 to 100

--- src/wgsim/sequence/option.cr
+++ src/wgsim/sequence/option.cr
@@ -6,8 +6,8 @@ module Wgsim
       property distance : Int32 = 500
       property std_deviation : Int32 = 50
       property average_depth : Float64 = 10
-      property size_left : Int32 = 70
-      property size_right : Int32 = 70
+      property size_left : Int32 = 100
+      property size_right : Int32 = 100
       property mutation_rate : Float64 = 0.001
       property indel_fraction : Float64 = 0.15
       property indel_extension_probability : Float64 = 0.3
